d7da6be8ee78b093b77798698aebc505a57ddbbe
Shun Chiang <shunchiang@Shuns-MacBook-Pro.local>
2020-03-27 12:16:21 -0500

    finished with stretch

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -9,10 +9,11 @@ const initialColor = {
 const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
+  const [add, setAdd] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
   const editColor = color => {
-    setEditing(true);
+    !add && setEditing(true);
     setColorToEdit(color);
   };
 
@@ -21,10 +22,36 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        updateColors(
+          colors.map(a => {
+            return a.id === colorToEdit.id ? res.data : a;
+          })
+        );
+      })
+      .catch(err => alert(err));
   };
 
-  const deleteColor = color => {
+  const saveAdd = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+      .post(`/api/colors`, colorToEdit)
+      .then(res => {
+        updateColors(res.data);
+      })
+      .catch(err => alert(err));
+  };
+
+  const deleteColor = id => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/api/colors/${id}`)
+      .then(res => {
+        updateColors(colors.filter(a => a.id !== res.data));
+      });
   };
 
   return (
@@ -34,12 +61,14 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color.id);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -50,6 +79,41 @@ const ColorList = ({ colors, updateColors }) => {
           </li>
         ))}
       </ul>
+      {!editing && <button onClick={() => setAdd(true)}>Add</button>}
+      {!editing && add && (
+        <form onSubmit={saveAdd}>
+          <legend>add color</legend>
+
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToEdit({ ...colorToEdit, color: e.target.value })
+              }
+              value={colorToEdit.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToEdit({
+                  ...colorToEdit,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToEdit.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit" onClick={saveAdd}>
+              save
+            </button>
+            <button onClick={() => setAdd(false)}>cancel</button>
+          </div>
+        </form>
+      )}
+
       {editing && (
         <form onSubmit={saveEdit}>
           <legend>edit color</legend>
@@ -75,7 +139,9 @@ const ColorList = ({ colors, updateColors }) => {
             />
           </label>
           <div className="button-row">
-            <button type="submit">save</button>
+            <button type="submit" onClick={saveEdit}>
+              save
+            </button>
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,60 @@
 import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+class Login extends React.Component {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", this.state.credentials)
+      .then(res => {
+        window.localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/bubblepage");
+      })
+      .catch(err => alert(err));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Welcome to the Bubble App!</h1>
+        <div className="login-fom">
+          <form onSubmit={this.handleSubmit}>
+            <h1>Log In</h1>
+            <input
+              type="text"
+              name="username"
+              value={this.state.credentials.username}
+              onChange={this.handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              value={this.state.credentials.password}
+              onChange={this.handleChange}
+            />
+            <button>Log In</button>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
 
 export default Login;
